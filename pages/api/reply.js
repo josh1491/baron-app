b8566fca3a18866fb99396daf42ad45d0cea2eb3
josh1491 <rathishjosh1491@gmail.com>
2025-11-17 22:14:04 +0800

    delete

--- pages/api/reply.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  const { message } = req.body;
-
-  res.status(200).json({
-    reply: "You said: " + message
-  });
-}
--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>Welcome to Baron App</h1>
-      <a href="/chat">Go to Chat</a>
-    </div>
-  );
-}
