0a1d049c42370cc7846fde84a30e088b2a29a114
josh1491 <rathishjosh1491@gmail.com>
2025-11-17 22:14:59 +0800

    Add Home component with welcome message

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Welcome to Baron App</h1>
+      <a href="/chat">Go to Chat</a>
+    </div>
+  );
+}
